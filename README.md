10a8adbf344f0a7df2710a182442a9cede48c850
Yasha <yashayash97@gmail.com>
2025-06-03 19:42:52 +0300

    composer

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This page wd16.local
+#  This page wd16.local
+[![Latest Version](https://img.shields.io/github/release/guzzle/guzzle.svg?style=flat-square)](https://github.com/Yasha-ship-it/wd16.local/tree/master)
--- app/registration/index.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="/app/registration/registration.php">
-    <input type="email" name="email" value="" placeholder="email">
-    <input type="password" name="password" placeholder="pass">
-    <input type="text" name="name" placeholder="name">
-    <input type="submit" value="регистрация">
-</form>
-<a href="/app/login/">войти</a>
-<?php
